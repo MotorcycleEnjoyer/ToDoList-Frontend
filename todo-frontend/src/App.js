22c5cc9f09bbb104a156b3ade7dc2401b39b73cf
MotorcycleEnjoyer <116019878+MotorcycleEnjoyer@users.noreply.github.com>
2022-10-20 11:18:14 -0400

    remove pointless state and comments

--- todo-frontend/src/App.js
+++ todo-frontend/src/App.js
@@ -5,9 +5,6 @@ import React from 'react';
 import AllToDoLists from './Components/AllToDoLists';
 
 function App() {
-  // HOMEPAGE, CREATING A NEW LIST, WITHIN A LIST
-  const [currentView, setCurrentView] = React.useState("homePage")
-
   const [defaultList, setDefaultList] = React.useState([
     {name: "First", list: ["one","two"]},
     {name: "Tying Shoes", list: ["Put shoes on","Cross the laces and pull", "make loops", "cross loops and pull"]},
@@ -20,36 +17,12 @@ function App() {
 
   /*   React.useEffect(()=>{
      axios.get("http://localhost:5000/", {crossDomain: true}).then(response => {
-      setStepList(response.data || [])
+      setDefaultList(response.data || [])
       }) 
 
       console.log("FETCH INITIAL LIST!!!")
   }, []) */
 
-  /*
-  //const [count, setCount] = React.useState(0)
-
-  React.useEffect(() => {
-    if(stepList.length === 0)
-      return
-
-
-      updateServerList(stepList)
-      /*
-      if(count >= 1){
-        console.log("WE ARE CHECKED")
-        
-      }
-      if(count <= 0){
-        setCount(prev => prev + 1)
-      }
-        
-
-      console.log("FETCH AFTER UPDATE!!!")
-
-        
-  },[stepList])
-  */
 
   /* 
   function updateServerList(data){
